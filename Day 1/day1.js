6beecc470bdcac26fa575d3a362966408b6a0b54
Kyle Thornton <kylemthornton@gmail.com>
2023-12-11 14:35:32 -0800

    firstAndLastWorking

--- Day 1/day1.js
+++ Day 1/day1.js
@@ -1,4 +1,18 @@
-import { input } from "./input";
+// import { input } from "./input";
 
 const testArray = ["1243khj234","3lkj6h34kjl","6345jk7h5"];
 
+const firstAndLastDigit = (string) => {
+    let intArray = []
+    for(const x of string) {
+        if(!isNaN(x)) {
+            intArray.push(x)
+        }
+    }
+    let ans = parseInt(intArray[0]) + parseInt(intArray[intArray.length-1])
+    return ans
+}
+
+for(let i = 0; i < testArray.length; i++){
+
+}
